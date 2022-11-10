505e3fae96d9cbe846b01d8a2a6fd18223e3eaca
mdbell <contact@mdbell.me>
2022-11-10 12:05:03 -0500

    JSON printing of MDB files

--- mdbtool/index.js
+++ mdbtool/index.js
@@ -1,12 +1,12 @@
 import yargs from "yargs"
+import MDBReader from "mdb-reader";
+import { readFileSync } from "fs";
 
 export function run() {
 
     const argv = yargs(process.argv)
         .help()
-        .command("json", "Prints out the contents of the mdb file as JSON", {
-
-        })
+        .command("json", "Prints out the contents of the mdb file as JSON")
         .option("file", {
             alias: 'f',
             description: "The source file",
@@ -16,16 +16,36 @@ export function run() {
         .alias("help", "h")
         .argv;
 
+    const buffer = readFileSync(argv.file);
+
     if (argv._.includes("json")) {
-        console.log(parseToJsonString(argv.file))
+        console.log(parseToJsonString(buffer))
     } else {
         console.error("Unsupported command given");
     }
 
 }
 
-export function parseToJsonString(fileName) {
+export function parseToObject(buffer) {
+    const reader = new MDBReader(buffer);
 
+    const res = {
+        names: reader.getTableNames(),
+        tables: {}
+    };
+
+    res.names.forEach(name => {
+        const table = reader.getTable(name);
+        res.tables[name] = {
+            column_names: table.getColumnNames(),
+            rows: table.getData()
+        };
+    })
+    return res;
+}
+
+export function parseToJsonString(buffer) {
+    return JSON.stringify(parseToObject(buffer));
 }
 
 run();
