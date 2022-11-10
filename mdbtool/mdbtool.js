f4fd4407016bdb2d469ae4075c54f95f8333a63e
mdbell <contact@mdbell.me>
2022-11-10 12:33:49 -0500

    refractored a bit, added option to store json as a file

--- mdbtool/mdbtool.js
+++ mdbtool/mdbtool.js
@@ -1,25 +1,35 @@
 import yargs from "yargs"
 import MDBReader from "mdb-reader";
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 
 export function run() {
 
     const argv = yargs(process.argv)
         .help()
+        .alias("help", "h")
         .command("json", "Prints out the contents of the mdb file as JSON")
-        .option("file", {
-            alias: 'f',
-            description: "The source file",
+        .option("input", {
+            alias: 'i',
+            description: "The path to the source mdb file",
             type: 'string',
             demandOption: true
         })
-        .alias("help", "h")
+        .option("output", {
+            alias: 'o',
+            description: " The path to the file where data will be output. If omitted the console is used instead"
+        })
+        .command("db <host> <port> <user> <pass>", "Adapts the data from the MDB file into the provided SQL database")
         .argv;
 
-    const buffer = readFileSync(argv.file);
+    const buffer = readFileSync(argv.input);
+
+    const logger = !argv.output || argv.output == "stdout" ? console.log : (str) => writeFileSync(argv.output, str);
 
     if (argv._.includes("json")) {
-        console.log(parseToJsonString(buffer))
+        const json = parseToJsonString(buffer);
+        logger(json);
+    } else if (argv._.includes("db")) {
+        console.log("TODO")
     } else {
         console.error("Unsupported command given");
     }
