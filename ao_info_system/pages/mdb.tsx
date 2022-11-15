ad651aad2bd4f862d3be40dd0ba6a3a8a172ada5
mdbell <contact@mdbell.me>
2022-11-15 17:19:13 -0500

    more tables

--- ao_info_system/pages/mdb.tsx
+++ ao_info_system/pages/mdb.tsx
@@ -24,13 +24,30 @@ interface Athlete {
     Last_name : string;
 }
 
-interface Divisions{
-    
+interface Division{
+    Div_name : string;
+    Div_no : number;
+}
+
+interface Entry{
+    Ath_no : number;
+    Comp_no : number;
+    Team_no : number;
+    Div_no : number;
+
+    Event_name : string;
+    Full_Eventname : string;
+    Event_gender : string;
+    Team_Abbr : string;
+    First_name : string;
+    Last_name : string;
 }
 
 function AthletesTable(props : TableProps){
     const rows = props.table.getData() as unknown as Athlete[];
-    return <table>
+    return <table style = {{
+        float: "left"
+    }}>
         <thead>
             <tr>
                 <th colSpan={5}>Athletes</th>
@@ -55,15 +72,83 @@ function AthletesTable(props : TableProps){
     </table>;
 }
 
+function DivisionsTable(props : TableProps){
+    const rows = props.table.getData() as unknown as Division[];
+    return <table style = {{
+        float: "left"
+    }}>
+        <thead>
+            <tr>
+                <th colSpan={5}>Divisions</th>
+            </tr>
+            <tr>
+                <th>ID #</th>
+                <th>Name</th>
+            </tr>
+        </thead>
+        <tbody>
+        {rows.map( (div) => <tr key = {div.Div_no}>
+            <td>{div.Div_no}</td>
+            <td>{div.Div_name}</td>
+        </tr>)}
+        </tbody>
+    </table>;
+}
+
+function EntriesTable(props : TableProps){
+    const rows = props.table.getData() as unknown as Entry[];
+    return <table style = {{
+        float: "left"
+    }}>
+        <thead>
+            <tr>
+                <th colSpan={9}>Entries</th>
+            </tr>
+            <tr>
+                <th colSpan={4}>IDs</th>
+                <th colSpan={6}>Data</th>
+            </tr>
+            <tr>
+                <th>Athlete #</th>
+                <th>Competition #</th>
+                <th>Team #</th>
+                <th>Division #</th>
+
+                <th>Event Name</th>
+                <th>Full Name</th>
+                <th>Gender</th>
+                <th>Team</th>
+                <th>Athlete Name</th>
+            </tr>
+        </thead>
+        <tbody>
+        {rows.map( (e, index) => <tr key = {index}>
+            <td>{e.Ath_no}</td>
+            <td>{e.Comp_no}</td>
+            <td>{e.Team_no}</td>
+            <td>{e.Div_no}</td>
+
+            <td>{e.Event_name}</td>
+            <td>{e.Full_Eventname}</td>
+            <td>{e.Event_gender}</td>
+            <td>{e.Team_Abbr}</td>
+            <td>{e.First_name} {e.Last_name}</td>
+        </tr>)}
+        </tbody>
+    </table>;
+}
+
 const RenderTable : NextPage<RenderProps> = (props) =>{
     const mdb = props.mdb as MDBReader;
     if(!mdb || Object.keys(mdb).length == 0){
         return <h1>Please select a file!</h1>;
     }
     console.log(mdb.getTableNames());
-    console.log(mdb.getTable("Athlete").getData());
+    console.log(mdb.getTable("Entries").getData());
     return <div>
+                <DivisionsTable table={ mdb.getTable("Divisions")} />
         <AthletesTable table= {mdb.getTable("Athlete")}/>
+        <EntriesTable table={mdb.getTable("Entries")}/>
     </div>;
 };
 
