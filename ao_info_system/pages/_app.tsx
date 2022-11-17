75e9fbae9b30cb34702f72e19e73ef3861ce6310
Matthew Bell <github@mdbell.me>
2022-11-17 18:39:10 -0500

    refractored a bit, and renamed to login

--- ao_info_system/pages/_app.tsx
+++ ao_info_system/pages/_app.tsx
@@ -3,14 +3,11 @@ import '../styles/globals.css'
 import "../styles/theme.less"
 
 import type { AppProps } from 'next/app'
-import Layout, { Content, Footer, Header } from 'antd/lib/layout/layout'
 
-import styles from "../styles/layout/main.module.css"
 
-import SiderComponent from '../components/layout/sider'
-import FooterComponent from '../components/layout/footer'
-import { NextComponentType, NextPage, NextPageContext } from 'next'
+import { NextPage } from 'next'
 import { ReactElement, ReactNode } from 'react'
+import { DefaultLayout } from '../components/layouts'
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -20,19 +17,8 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
-function getDefaultLayout(page : ReactNode){
-  return <Layout className={styles.layout} hasSider>
-  <SiderComponent />
-  <Layout>
-    <Header>header</Header>
-    <Content className={styles.content}>{page}</Content>
-    <FooterComponent />
-  </Layout>
-</Layout>
-}
-
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? getDefaultLayout;
+  const getLayout = Component.getLayout ?? DefaultLayout;
   return getLayout(<Component {...pageProps} />)
 }
 
